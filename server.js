ecff8ff1548d3593587911898d197b1e8b728b5b
Agustin Pina <aguspina87@gmail.com>
2015-02-01 22:06:13 -0300

    Add express-response

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var Response = require('express-response');
 var app = express();
 var fs = require('fs');
 var compiler = require('./file-test-compiler');
@@ -10,6 +11,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.post('/test', urlencodedParser, function (req, res) {
   var path = compiler.createCompilationFile(req.body);
   runner.runTestFile(path);
+  Response.OK(res)({"exit":"passed","out":"All is passing"});
 })
 
 var server = app.listen(8080, function () {
