24520cdb83af8770d0820c0d03b7410b4e4944fb
Agustin Pina <aguspina87@gmail.com>
2015-02-04 00:50:36 -0300

    Add content submission for file

--- file-test-compiler.js
+++ file-test-compiler.js
@@ -3,7 +3,7 @@ var tmp = require('tmp');
 
 function createCompilationFile(body) {
   var require = "var assert = require('assert');";
-  fs.appendFile('test.js', require+'\n'+'\n'+body.test, function (err) {
+  fs.appendFile('test.js', require+'\n'+body.content+'\n'+body.test, function (err) {
     });
 }
 exports.createCompilationFile = createCompilationFile;
