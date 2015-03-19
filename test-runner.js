9b51eafe56f260a0ebe632d9aea52c1f9a5924dd
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-03-19 17:09:09 -0300

    Using callback

--- test-runner.js
+++ test-runner.js
@@ -1,24 +1,24 @@
 var child_process = require('child_process');
 var fs = require('fs');
- 
-function runTestFile() {
+
+function runTestFile(done) {
 // Run the command in a subshell
 child_process.exec("mocha -R reporter-file test.js" + ' 2>&1 1>output && echo done! > done');
- 
+
 // Block the event loop until the command has executed.
 while (!fs.existsSync('output') && !fs.existsSync('xunit.xml')) {
 // Do nothing
 }
- 
+
 // Read the output
 var output = fs.readFileSync('output');
 console.log(String(output));
- 
+
 // Delete temporary files.
 
 
- 
-return output;
+
+done(output);
 }
 
 exports.runTestFile = runTestFile;
