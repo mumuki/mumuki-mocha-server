adb2ebb6a0b4fdb3784c3f4ce8b66dd707be600b
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-03-19 17:17:32 -0300

    Avoiding race condition

--- test-runner.js
+++ test-runner.js
@@ -3,22 +3,14 @@ var fs = require('fs');
 
 function runTestFile(done) {
 // Run the command in a subshell
-child_process.exec("mocha -R reporter-file test.js" + ' 2>&1 1>output && echo done! > done');
-
-// Block the event loop until the command has executed.
-while (!fs.existsSync('output') && !fs.existsSync('xunit.xml')) {
-// Do nothing
-}
-
-// Read the output
-var output = fs.readFileSync('output');
-console.log(String(output));
-
-// Delete temporary files.
-
-
-
-done(output);
+child_process.exec(
+  'mocha -R reporter-file test.js 2>&1 1>output && echo done! > done', function(){
+    // Read the output
+    var output = fs.readFileSync('output');
+    console.log(String(output));
+    // Delete temporary files.
+    done(output);
+  });
 }
 
 exports.runTestFile = runTestFile;
