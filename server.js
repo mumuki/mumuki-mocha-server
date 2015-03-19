691453e6f78b7ddc7bd5bb684f1735b2ee68564c
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-03-19 17:55:47 -0300

    Fixing require order

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 var express = require('express');
-var Response = require('express-response');
+var bodyParser = require('body-parser')
 
 var app = express();
-var fs = require('fs');
+
+var jsonParser = bodyParser.json({ type: 'application/*' });
+
+var Response = require('express-response');
 
 var compiler = require('./test-compiler');
 var runner = require('./test-runner');
 
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-var parseString = require('xml2js').parseString;
-var childProcess = require('child_process');
-
-app.post('/test', urlencodedParser, function (req, res) {
+app.post('/test', jsonParser, function (req, res) {
   compiler.createCompilationFile(req.body);
-  var result2 = "";
-  var output2 = "";
-
-  runner.runTestFile(function(result){
-    Response.OK(res)(result);
+  runner.runTestFile(function(result) {
     fs.unlinkSync("test.js");
+    Response.OK(res)(result);
   });
 })
 
