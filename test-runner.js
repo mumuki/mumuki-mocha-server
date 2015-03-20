062aacd4115ea658398ea4e79875bfab8675c1e9
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-03-20 01:45:54 -0300

    Running mocha without colors

--- test-runner.js
+++ test-runner.js
@@ -1,9 +1,8 @@
 var child_process = require('child_process');
 var fs = require('fs');
 
-function runTestFile(done) {
-// Run the command in a subshell
-child_process.exec('mocha test.js', function(error, stdout, stderr){
+function runTestFile(file, done) {
+  child_process.exec('mocha -C ' + file, function(error, stdout, stderr){
     done({
       exit: error ? 'failed' : 'passed',
       out: stdout + stderr
