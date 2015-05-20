49bd474717acce3cf5f1ece859149fa0eaef4834
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-05-20 17:20:20 -0300

    Processing extra src

--- test-compiler.js
+++ test-compiler.js
@@ -3,10 +3,19 @@ var tmp = require('tmp');
 
 function createCompilationFile(body, done) {
   tmp.file(function(err, path, fd) {
-    var requires = "var assert = require('assert');";
-    fs.writeFile(path, requires +'\n'+ body.content + '\n' + body.test, function(){
+    fs.writeFile(path, compile(body), function(){
       done(path);
     });
   });
 }
+
+function compile(body) {
+  var requires = "var assert = require('assert');";
+
+  return requires +'\n'+
+         body.extra + '\n' +
+         body.content + '\n' +
+         body.test
+}
+
 exports.createCompilationFile = createCompilationFile;
