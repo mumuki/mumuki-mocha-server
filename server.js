5ec322676aaf293ac5ec6ac7dc3e5d8a3ee95fcc
Federico Scarpa <fedescarpa@gmail.com>
2015-06-29 21:04:27 -0300

    fixing all linter errors

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+'use strict';
+
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -9,14 +11,14 @@ var compiler = require('./test-compiler');
 var runner = require('./test-runner');
 
 app.post('/test', jsonParser, function (req, res) {
-  compiler.createCompilationFile(req.body, function(file){
-    runner.runTestFile(file, function(result) {
+  compiler.createCompilationFile(req.body, function (file) {
+    runner.runTestFile(file, function (result) {
       res.send(JSON.stringify(result));
     });
   });
-})
+});
 
 var port = process.env.PORT || 8080;
 var server = app.listen(port, function () {
-  console.log("Serving on port " + port);
-})
+  console.log('Serving on port ' + port);
+});
--- test-runner.js
+++ test-runner.js
@@ -1,8 +1,10 @@
+'use strict';
+
 var child_process = require('child_process');
 var fs = require('fs');
 
 function runTestFile(file, done) {
-  child_process.exec('mocha -C ' + file, function(error, stdout, stderr){
+  child_process.exec('mocha -C ' + file, function (error, stdout, stderr) {
     done({
       exit: error ? 'failed' : 'passed',
       out: stdout + stderr
