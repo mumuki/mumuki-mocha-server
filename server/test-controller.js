c51f454088e24484495397fdb2d589b3fb895394
Federico Scarpa <fedescarpa@gmail.com>
2015-07-02 02:20:22 -0300

    deleting temp file when response was sent

--- server/test-controller.js
+++ server/test-controller.js
@@ -4,6 +4,17 @@ var Bluebird = require('bluebird');
 
 var compiler = require('./test-compiler');
 var runner = require('./test-runner');
+var del = require('del');
+
+var delAsync = Bluebird.promisify(del);
+
+var sendResult = function (testReport) {
+  this.status(200).json(testReport.result);
+};
+
+var deleteTempFile = function (testReport) {
+  return delAsync(testReport.filepath);
+};
 
 module.exports = {
 
@@ -11,8 +22,8 @@ module.exports = {
     return Bluebird.resolve(req.body)
       .then(compiler.createCompilationFile)
       .then(runner.runTestFile)
-      .then(res.status(200).json.bind(res));
+      .tap(sendResult.bind(res))
+      .tap(deleteTempFile);
   }
 
 };
-
