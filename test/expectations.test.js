c6d613665bad0876c5b79e910a3bc27e7a974ccf
Federico Scarpa <fedescarpa@gmail.com>
2015-07-03 14:28:27 -0300

    fix linter

--- test/expectations.test.js
+++ test/expectations.test.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var should = require('should');
 
 var esprima = require('esprima');
@@ -10,15 +11,15 @@ function match(patterns) {
   return function(it) {
     return patterns.some(function(pattern) {
       return  j.unify(pattern, it);
-    })
-  }
+    });
+  };
 }
 
 function identifier(binding) {
   return { type: 'Identifier', name: binding };
 }
 
-function HasBinding(binding, ast) {
+function hasBinding(binding, ast) {
   return declarationsOf(ast).some(match([
     { type: 'FunctionDeclaration', id: identifier(binding), _: j._ },
     {
@@ -38,32 +39,32 @@ function declarationsOf(ast) {
 }
 
 function p(code) {
-  return esprima.parse(code)
+  return esprima.parse(code);
 }
 
-describe('HasBinding', function () {
+describe('hasBinding', function () {
 
-  it("hasBinding when function declaration exists for binding", function(){
-    assert(HasBinding('foo', p('function foo(){}')));
-  })
+  it('hasBinding when function declaration exists for binding', function(){
+    assert(hasBinding('foo', p('function foo(){}')));
+  });
 
-  it("not hasBinding when function declaration exist for other binding", function(){
-    assert(!HasBinding('bar', p('function foo(){}')));
-  })
+  it('not hasBinding when function declaration exist for other binding', function(){
+    assert(!hasBinding('bar', p('function foo(){}')));
+  });
 
-  it("not hasBinding when code is empty", function(){
-    assert(!HasBinding('bar', p('')));
-  })
+  it('not hasBinding when code is empty', function(){
+    assert(!hasBinding('bar', p('')));
+  });
 
-  it("hasBinding when code has variable for binding", function(){
-    assert(HasBinding('bar', p('var bar = 4')));
-  })
+  it('hasBinding when code has variable for binding', function(){
+    assert(hasBinding('bar', p('var bar = 4')));
+  });
 
-  it("not hasBinding when code has variable for other binding", function(){
-    assert(!HasBinding('bar', p('var foo = 4')));
-  })
+  it('not hasBinding when code has variable for other binding', function(){
+    assert(!hasBinding('bar', p('var foo = 4')));
+  });
 
-  it("not hasBinding when code has expression", function(){
-    assert(!HasBinding('bar', p('5 + 2')));
-  })
+  it('not hasBinding when code has expression', function(){
+    assert(!hasBinding('bar', p('5 + 2')));
+  });
 });
