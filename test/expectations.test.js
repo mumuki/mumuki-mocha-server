b164548a749ac7abaccedab573e69710cdbbb286
Federico Scarpa <fedescarpa@gmail.com>
2015-07-05 14:08:58 -0300

    supporting multiple varibale declarators

--- test/expectations.test.js
+++ test/expectations.test.js
@@ -1,11 +1,22 @@
 'use strict';
 
+Array.prototype.concatMap = function (f) {
+  return Array.prototype.concat.apply([], this.map(f));
+};
+
 var should = require('should');
 
 var esprima = require('esprima');
 var j = require('junify');
 
 var assert = require('assert');
+
+function hasBinding(binding, ast) {
+  return declarationsOf(ast).some(match([
+    { type: 'FunctionDeclaration', id: identifier(binding), _: j._ },
+    { type: 'VariableDeclarator', id: identifier(binding), init: j._ }
+  ]));
+}
 
 function match(patterns) {
   return function(it) {
@@ -19,23 +30,16 @@ function identifier(binding) {
   return { type: 'Identifier', name: binding };
 }
 
-function hasBinding(binding, ast) {
-  return declarationsOf(ast).some(match([
-    { type: 'FunctionDeclaration', id: identifier(binding), _: j._ },
-    {
-      type: 'VariableDeclaration',
-      declarations: [{
-        type: 'VariableDeclarator',
-        id: identifier(binding),
-        init: j._ }],
-        _: j._
-      }]));
-}
-
 function declarationsOf(ast) {
-  return ast.body.filter(match([
-    {type: 'VariableDeclaration', _: j._},
-    {type: 'FunctionDeclaration', _: j._}]));
+  return ast.body.concatMap(function (node) {
+    if (j.unify({ type: 'FunctionDeclaration', _: j._ }, node)) {
+      return [node];
+    } else if (j.unify({ type: 'VariableDeclaration', _: j._ }, node)) {
+      return node.declarations;
+    } else {
+      return [];
+    }
+  });
 }
 
 function p(code) {
@@ -67,4 +71,25 @@ describe('hasBinding', function () {
   it('not hasBinding when code has expression', function(){
     assert(!hasBinding('bar', p('5 + 2')));
   });
+
+  it('hasBinding for first declarator when code has multiple varible declarators', function(){
+    assert(hasBinding('a', p('var a = 5, b = 2;')));
+  });
+
+  it('hasBinding for second declarator when code has multiple varible declarators', function(){
+    assert(hasBinding('b', p('var a = 5, b = 2;')));
+  });
+
+});
+
+describe('concatMap', function () {
+
+  it('concatMap', function(){
+
+    var concatMap = [{foo: [1,2]}, {foo: [3,4]}].concatMap(function (obj) { return obj.foo; });
+
+    concatMap.should.be.eql([1,2,3,4]);
+
+  });
+
 });
