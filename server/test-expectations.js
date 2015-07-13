7f42e17a828f7c38831a0e50d6812bfaabc51313
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-13 19:12:38 -0300

    Fix: not crashing server on esprima parsing error

--- server/test-expectations.js
+++ server/test-expectations.js
@@ -4,18 +4,32 @@ var esprima = require('esprima');
 
 var expectationChecker = require('./expectation-checker');
 
+function checkExpectations(expectations, f) {
+  return expectations.map(function (expectation) {
+    return {
+      result: f(expectation),
+      expectation: expectation
+    };
+  })
+}
+
+function checkerFor(content) {
+  try {
+    var ast = esprima.parse(content);
+    return function (e) {
+      return expectationChecker.isValid(ast, e);
+    };
+  } catch (err) {
+    return function () {
+      return false;
+    };
+  }
+}
+
 module.exports = {
 
   check: function (content, expectations) {
-
-    var ast = esprima.parse(content);
-
-    return expectations.map(function (expectation) {
-      return {
-        result: expectationChecker.isValid(ast, expectation),
-        expectation: expectation
-      };
-    });
+    return checkExpectations(expectations, checkerFor(content));
   }
 
 };
